62e70364e7002d38b48b4395c1a057317cd71724
Hamza Siddiqui <80615079+hamzapro305@users.noreply.github.com>
2024-08-22 03:15:56 +0500

    v1.1

--- src/utils/LexicalAnalyzer.ts
+++ src/utils/LexicalAnalyzer.ts
@@ -1,34 +1,61 @@
 import { BREAKERS, PUNCTUATION } from "./CONSTANTS";
 
+type WordT = {
+    word: string;
+    lineNo: number;
+};
 const GenerateWords = (code: string) => {
-    let w: string[] = [];
+    let w: WordT[] = [];
     let temp: string = "";
-    for (let i = 0; i < code.length; i++) {
-        const char = code[i];
-        if (PUNCTUATION.includes(char)) {
-            w.push(temp);
-            w.push(char);
-            temp = "";
-        } else if (BREAKERS.includes(char)) {
-            if (temp) w.push(temp);
-            if (char !== " " && char !== "\n") {
+
+    const Lines = code.split("\n").filter((x) => x !== "");
+    // let isUnderComment = false
+
+    for (let row = 0; row < Lines.length; row++) {
+        const line = Lines[row];
+        for (let i = 0; i < line.length; i++) {
+            const char = line[i];
+            if (temp == "/" && char == "/") {
+                break;
+            }
+            else if (PUNCTUATION.includes(char)) {
+                w.push({ word: temp, lineNo: row });
+                w.push({ word: char, lineNo: row });
+                temp = "";
+            } else if (BREAKERS.includes(char)) {
+                w.push({ word: temp, lineNo: row });
                 temp = char;
             } else {
-                temp = "";
+                temp += char;
             }
-        } else {
-            temp += char;
         }
+        w.push({ word: "/n", lineNo: row });
     }
-    return w;
+
+    return w.filter((x) => {
+        return x.word !== " " && x.word !== "";
+    });
+};
+
+const GenerateTokens = (Words: WordT[]) => {
+    let _tokens: {}[] = [];
+    Words.forEach((item) => {
+        _tokens.push({
+            tokenClass: "invalid",
+            word: item.word,
+            lineNo: item.lineNo,
+        });
+    });
+    return _tokens;
 };
 
 export const LexicalAnalyzer = (code: string) => {
     const Words = GenerateWords(code);
+    const Tokens = GenerateTokens(Words);
 
     return JSON.stringify(
         {
-            words: Words,
+            Tokens: Tokens,
         },
         null,
         2
