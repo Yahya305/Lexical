a6686baaf3e948f847456a37ce877efaea96226c
Hamza Siddiqui <80615079+hamzapro305@users.noreply.github.com>
2024-08-22 02:46:08 +0500

    v1

--- src/utils/CONSTANTS.ts
+++ src/utils/CONSTANTS.ts
@@ -55,6 +55,7 @@ export const BREAKERS = [
     "\r",
     '"',
     "'",
+    ".",
     ...PUNCTUATION,
     ...OPERATORS,
 ];
--- src/utils/LexicalAnalyzer.ts
+++ src/utils/LexicalAnalyzer.ts
@@ -1,11 +1,15 @@
-import { BREAKERS } from "./CONSTANTS";
+import { BREAKERS, PUNCTUATION } from "./CONSTANTS";
 
 const GenerateWords = (code: string) => {
     let w: string[] = [];
     let temp: string = "";
     for (let i = 0; i < code.length; i++) {
         const char = code[i];
-        if (BREAKERS.includes(char)) {
+        if (PUNCTUATION.includes(char)) {
+            w.push(temp);
+            w.push(char);
+            temp = "";
+        } else if (BREAKERS.includes(char)) {
             if (temp) w.push(temp);
             if (char !== " " && char !== "\n") {
                 temp = char;
