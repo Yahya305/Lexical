bb424a68e2af5da81cc6fc5cca6d21a499c00aad
Hamza Siddiqui <80615079+hamzapro305@users.noreply.github.com>
2024-08-22 03:22:43 +0500

    v1.2

--- src/utils/LexicalAnalyzer.ts
+++ src/utils/LexicalAnalyzer.ts
@@ -4,58 +4,63 @@ type WordT = {
     word: string;
     lineNo: number;
 };
-const GenerateWords = (code: string) => {
-    let w: WordT[] = [];
+
+const GenerateWords = (code: string): WordT[] => {
+    let words: WordT[] = [];
     let temp: string = "";
 
-    const Lines = code.split("\n").filter((x) => x !== "");
-    // let isUnderComment = false
+    const Lines = code.split("\n");
 
     for (let row = 0; row < Lines.length; row++) {
         const line = Lines[row];
         for (let i = 0; i < line.length; i++) {
             const char = line[i];
-            if (temp == "/" && char == "/") {
-                break;
-            }
-            else if (PUNCTUATION.includes(char)) {
-                w.push({ word: temp, lineNo: row });
-                w.push({ word: char, lineNo: row });
+            if (PUNCTUATION.includes(char)) {
+                if (temp.trim()) {
+                    words.push({ word: temp.trim(), lineNo: row + 1 });
+                }
+                words.push({ word: char, lineNo: row + 1 });
                 temp = "";
             } else if (BREAKERS.includes(char)) {
-                w.push({ word: temp, lineNo: row });
+                if (temp.trim()) {
+                    words.push({ word: temp.trim(), lineNo: row + 1 });
+                }
                 temp = char;
+                words.push({ word: temp, lineNo: row + 1 });
+                temp = "";
             } else {
                 temp += char;
             }
         }
-        w.push({ word: "/n", lineNo: row });
+        if (temp.trim()) {
+            words.push({ word: temp.trim(), lineNo: row + 1 });
+            temp = "";
+        }
+        words.push({ word: "\n", lineNo: row + 1 });
     }
 
-    return w.filter((x) => {
-        return x.word !== " " && x.word !== "";
-    });
+    return words.filter((x) => x.word !== "" && x.word !== " ");
 };
 
-const GenerateTokens = (Words: WordT[]) => {
-    let _tokens: {}[] = [];
-    Words.forEach((item) => {
-        _tokens.push({
-            tokenClass: "invalid",
+const GenerateTokens = (words: WordT[]) => {
+    let tokens: {}[] = [];
+    words.forEach((item) => {
+        tokens.push({
+            // tokenClass: "invalid",
             word: item.word,
             lineNo: item.lineNo,
         });
     });
-    return _tokens;
+    return tokens;
 };
 
-export const LexicalAnalyzer = (code: string) => {
-    const Words = GenerateWords(code);
-    const Tokens = GenerateTokens(Words);
+export const LexicalAnalyzer = (code: string): string => {
+    const words = GenerateWords(code);
+    const tokens = GenerateTokens(words);
 
     return JSON.stringify(
         {
-            Tokens: Tokens,
+            Tokens: tokens,
         },
         null,
         2
